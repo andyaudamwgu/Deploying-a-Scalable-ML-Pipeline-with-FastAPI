d20815cbcc99e50c70e81610398d2e1554388006
Mien Au <mienau@Miens-MBP.attlocal.net>
2025-02-25 22:02:07 -0600

    Fix flake8 errors and complete TODOs in test_ml.py and train_model.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,30 @@
-import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+from ml.model import train_model, inference, compute_model_metrics
+import numpy as np
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+def test_train_model():
+    """Test that train_model returns a fitted model."""
+    X_train = np.array([[1, 2], [3, 4]])
+    y_train = np.array([0, 1])
+    model = train_model(X_train, y_train)
+    assert model is not None, "Model should not be None"
+    assert hasattr(model, 'predict'), "Model should have predict method"
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
-    """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+def test_inference():
+    """Test that inference returns predictions of correct length."""
+    X_test = np.array([[1, 2], [3, 4]])
+    y_train = np.array([0, 1])
+    model = train_model(X_test, y_train)
+    preds = inference(model, X_test)
+    assert len(preds) == len(X_test), "Predictions length should match input"
+
+
+def test_compute_metrics():
+    """Test that compute_model_metrics returns valid scores."""
+    y = np.array([0, 1, 0, 1])
+    preds = np.array([0, 1, 1, 0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert 0 <= precision <= 1, "Precision should be between 0 and 1"
+    assert 0 <= recall <= 1, "Recall should be between 0 and 1"
+    assert 0 <= fbeta <= 1, "Fbeta should be between 0 and 1"
